3ae376972db4cb6892f5bcb0b04b67fbbcd34651
DaBigRed <131016808+DaBigRed@users.noreply.github.com>
2023-12-04 08:51:15 -0600

    Update billboardcrawler.py
    in the beninging

--- billboardcrawler.py
+++ billboardcrawler.py
@@ -1,1 +1,12 @@
-import scrapy #possible crawler/scraper to use
+#import scrapy #possible crawler/scraper to use
+from urllib.request import urlopen
+url = "https://www.reddit.com"
+response = urlopen(url)
+data = response.read()
+dataList = data.split()
+linkList = []
+for data in dataList:
+  if "http" in data:
+    linkList.append(data)
+
+    
