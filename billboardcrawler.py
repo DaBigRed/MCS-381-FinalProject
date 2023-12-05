96a5cefd19c4b4e7a557a21304aa1143a42a4e6f
DaBigRed <131016808+DaBigRed@users.noreply.github.com>
2023-12-05 09:11:41 -0600

    Update billboardcrawler.py

--- billboardcrawler.py
+++ billboardcrawler.py
@@ -1,12 +1,48 @@
 #import scrapy #possible crawler/scraper to use
 from urllib.request import urlopen
-url = "https://www.reddit.com"
-response = urlopen(url)
-data = response.read()
-dataList = data.split()
-linkList = []
-for data in dataList:
-  if "http" in data:
-    linkList.append(data)
 
+class page:
+  def __init__ (self, url):
+    self.url = url
+    incoming = []
+    outgoing = []
+    oldPR = 0
+    newPR = 0
+  def getURL():
+    return self.url
+  def addIncoming(newURL):
+    incoming += newURL
+  def addOutgoing(newURL):
+    outgoing += newURL
     
+def getLinks(url, pageList):
+  response = urlopen(url)
+  data = response.read()
+  dataList = data.split()
+  linkList = []
+  pageURLList = []
+  for page in pageList:
+    pageURLList.append(page.getURL())
+  for data in dataList:
+    if "http" in data:
+      if data not in pageURLList:
+        linkList.append(data)
+  return linkList
+
+pageList = []
+while pageList.size() < 50000:
+  allLinks = []
+  url = "https://www.reddit.com"
+  allLinks += getLinks(url, allLinks)
+  firstPage = Page(url)
+  firstPage.addOutgoing(allLinks)
+  for link in allLinks:
+    tempLinks = getLinks(link, allLinks) 
+    allLinks += tempLinks
+    #going to have to add an "if doesnt exist"  in here somewhere
+    tempPage = Page(link)
+    tempPage.addOutgoing(tempLinks)
+    pageList.append(tempPage)
+    #add keep track of all outgoing links
+    allLinks.remove(link)
+
