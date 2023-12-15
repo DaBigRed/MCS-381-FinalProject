21104891e2ebde2edf873f133740dc31e087552e
DaBigRed <131016808+DaBigRed@users.noreply.github.com>
2023-12-14 20:07:53 -0600

    Update betweenness.py

--- betweenness.py
+++ betweenness.py
@@ -1,6 +1,29 @@
 import json
-def findPaths(data, node1, node2):
-  return recFindPath(data, node1, node2, node1, [])
+def findPaths(data, node1, node2): 
+  #Find all paths
+  paths = recFindPath(data, node1, node2, node1, [])
+  min = len(paths[0])
+  max = 0
+  for path in paths:
+    #If the path did not reach the desired value
+    if(path[-1] == -1):
+      #remove it
+      paths.remove(path)
+    #else if length is less than min
+    else if(len(path) < min):
+      #set new min
+      min = len(path)
+    #else if length is greater than max
+    else if(len(path) > max):
+      #set new max
+      max = len(path)
+  for path in paths:
+    #If a path's length is longer than the min
+    if(len(path) != min):
+      #remove it from paths
+      paths.remove(path)
+  #return all paths that are the minimum length
+  return paths
 #Recursive
 def recFindPaths(data, node1, node2, currNode, pastNodes):
   #if destination is already is a direct connection to current node
@@ -23,17 +46,30 @@ def recFindPaths(data, node1, node2, currNode, pastNodes):
     paths = []
     #for all the nodes not already in path
     for node in newNodes:
+      #create path with node in the final spot
       nodebuilder = pastNodes + [currNode] + recFindPaths(data, node1, node2, node, [pastNodes + currNode]) 
-      pastNodes.append(nodeBuilder)
+      #Add this to our sum of paths
       paths.append(pastNodes.append(currNode))
     return paths
 #Save dictionary with url as keys and number of appearances in shortest paths as values, variable name = data
 with open("graph.json", "r") as f:
   data = json.load(f)
+dict = {}
+paths = [] 
+#Find shortest paths of all 1-2 node pairs
 for page in data:
   for page2 in data:
-    findPaths(data, page1, page2)
-      
+    paths += findPaths(data, page1, page2)
+#initialize all keys at 0
+for key in data:
+  dict[key] = 0
+#Add 1 for everytime a stop appears in a path
+for path in paths:
+  for stop in path:
+    dict[stop] += 1
+  
+
+
 
 #Export new dictionary to a json file
 with open("betweenness.json", "w") as json_file:
